e70ec9a7b69e711be8e8bc51561751d198e2b2f2
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-19 21:16:02 +0700

    fix status bar

--- final-project/App.js
+++ final-project/App.js
@@ -1,6 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
 import Navbar from "./app/components/Navbar";
 
 export default function App() {
-  return <Navbar />;
+  return (
+    <View style={styles.container}>
+      <Navbar />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
