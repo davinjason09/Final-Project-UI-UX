7091775cb79f28378eaaf6bb75d07307d08eb966
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-22 23:14:53 +0700

    update App.js and package.json

--- final-project/App.js
+++ final-project/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { registerRootComponent } from "expo";
 
 import Navbar from "./app/components/Navbar";
 
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+registerRootComponent(App);
