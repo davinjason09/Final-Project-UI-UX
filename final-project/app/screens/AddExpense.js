c3e355de16b2f190e361fd26e9173847ac9649ba
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-23 22:00:15 +0700

    added input field

--- final-project/app/screens/AddExpense.js
+++ final-project/app/screens/AddExpense.js
@@ -1,10 +1,61 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 
 export default function AddExpense() {
   return (
     <View style={styles.base}>
-      <Text>AddExpense</Text>
+      <View style={{ marginTop: 38 }}>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Date
+          </Text>
+          <TextInput
+            placeholder="dd/mm/yy"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Category
+          </Text>
+          <TextInput
+            placeholder="Choose Category"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Account
+          </Text>
+          <TextInput
+            placeholder="Choose Account"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Amount
+          </Text>
+          <TextInput
+            placeholder="Insert Amount"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Note
+          </Text>
+          <TextInput
+            placeholder="Add Note"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +63,25 @@ export default function AddExpense() {
 const styles = StyleSheet.create({
   base: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#F7F8F9",
+  },
+  inputBox: {
+    backgroundColor: "#FFFFFF",
+    flexDirection: "row",
+    borderRadius: 5,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "86.4%",
+    height: 56,
+    elevation: 5,
+    shadowColor: "#000000",
+    marginBottom: 5,
+  },
+  textInput: {
+    fontSize: 12,
+    marginRight: 25,
+    width: "40%",
+    textAlign: "right",
   },
 });
--- final-project/app/screens/AddIncome.js
+++ final-project/app/screens/AddIncome.js
@@ -1,10 +1,52 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { TextInput } from "react-native-gesture-handler";
 
 export default function AddIncome() {
   return (
     <View style={styles.base}>
-      <Text>AddIncome</Text>
+      <View style={{ marginTop: 38 }}>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Date
+          </Text>
+          <TextInput
+            placeholder="dd/mm/yy"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Account
+          </Text>
+          <TextInput
+            placeholder="Choose Account"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Amount
+          </Text>
+          <TextInput
+            placeholder="Insert Amount"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={{ fontSize: 14, fontWeight: 400, marginLeft: 25 }}>
+            Note
+          </Text>
+          <TextInput
+            placeholder="Add Note"
+            placeholderTextColor={"#9B9B9B"}
+            style={styles.textInput}
+          ></TextInput>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +54,25 @@ export default function AddIncome() {
 const styles = StyleSheet.create({
   base: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#F7F8F9",
+  },
+  inputBox: {
+    backgroundColor: "#FFFFFF",
+    flexDirection: "row",
+    borderRadius: 5,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "86.4%",
+    height: 56,
+    elevation: 5,
+    shadowColor: "#000000",
+    marginBottom: 5,
+  },
+  textInput: {
+    fontSize: 12,
+    marginRight: 25,
+    width: "40%",
+    textAlign: "right",
   },
 });
