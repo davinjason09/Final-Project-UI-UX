a640d786885e3a3758c7c9ddffd088b08651abb3
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-23 21:20:22 +0700

    Create RecentTransaction.js

---
+++ final-project/app/components/RecentTransaction.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { FlatList } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+export default function RecentTransaction() {
+  const nav = useNavigation();
+
+  const EmptyListMessage = () => {
+    return (
+      <View style={styles.emptyListStyle}>
+        <Text>No Data Found</Text>
+      </View>
+    );
+  };
+
+  return (
+    <View>
+      <View style={styles.title}>
+        <Text style={{ fontSize: 20, fontWeight: 500 }}>
+          Recent Transactions
+        </Text>
+        <Text
+          style={{ fontSize: 14, fontWeight: 500, color: "#2340DC" }}
+          onPress={() => nav.navigate("Transaction Details")}
+        >
+          Show all
+        </Text>
+      </View>
+      {/* <FlatList ListEmptyComponent={EmptyListMessage} /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    marginLeft: 39,
+    marginRight: 42,
+    marginTop: 15,
+  },
+  transactions: {
+    backgroundColor: "#FFFFFF",
+    width: "85%",
+    minHeight: 200,
+    marginTop: 15,
+    marginBottom: 68,
+    flexDirection: "row",
+    flexGrow: 1,
+    alignSelf: "center",
+    borderRadius: 30,
+    elevation: 3,
+    shadowColor: "#000000",
+  },
+});
