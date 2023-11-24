f6ebba2702b8fdee40913268560c937b51ae6baa
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-25 00:03:35 +0700

    Added interactibility

--- final-project/app/screens/AddExpense.js
+++ final-project/app/screens/AddExpense.js
@@ -1,40 +1,101 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+  Keyboard,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+import ContentPicker from "../components/ContentPicker";
+import Account from "../components/Account";
+import Category from "../components/Category";
 
 export default function AddExpense() {
+  const [isPickerVisible, setPickerVisible] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState("");
+  const [isCategoryModalVisible, setCategoryModalVisible] = useState(false);
+  const [isAccountModalVisible, setAccountModalVisible] = useState(false);
+  const [pickedCategories, setPickedCategories] = useState(null);
+  const [pickedAccount, setPickedAccount] = useState(null);
+
+  const openCategoryModal = () => setCategoryModalVisible(true);
+  const closeCategoryModal = () => setCategoryModalVisible(false);
+
+  const openAccountModal = () => setAccountModalVisible(true);
+  const closeAccountModal = () => setAccountModalVisible(false);
+
+  const showDatePicker = () => {
+    setPickerVisible(true);
+    Keyboard.dismiss();
+  };
+
+  const hideDatePicker = () => setPickerVisible(false);
+
+  const handleCategorySelect = (category) => setPickedCategories(category);
+  const handleAccountSelect = (account) => setPickedAccount(account);
+  const handleDateConfirm = (selectedDate) => {
+    hideDatePicker();
+    setDate(selectedDate);
+    setSelectedDate(
+      `${String(selectedDate.getDate()).padStart(2, "0")}/${String(
+        selectedDate.getMonth() + 1
+      ).padStart(2, "0")}/${String(selectedDate.getFullYear()).slice(2)}`
+    );
+  };
+
+  const nav = useNavigation();
+
   return (
     <View style={styles.base}>
       <View style={{ marginTop: 38 }}>
-        <View style={styles.inputBox}>
-          <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
-            Date
-          </Text>
-          <TextInput
-            placeholder="dd/mm/yy"
-            placeholderTextColor={"#9B9B9B"}
-            style={styles.textInput}
-          ></TextInput>
-        </View>
-        <View style={styles.inputBox}>
-          <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
-            Category
-          </Text>
-          <TextInput
-            placeholder="Choose Category"
-            placeholderTextColor={"#9B9B9B"}
-            style={styles.textInput}
-          ></TextInput>
-        </View>
-        <View style={styles.inputBox}>
-          <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
-            Account
-          </Text>
-          <TextInput
-            placeholder="Choose Account"
-            placeholderTextColor={"#9B9B9B"}
-            style={styles.textInput}
-          ></TextInput>
-        </View>
+        <Pressable onPress={showDatePicker}>
+          <View style={styles.inputBox}>
+            <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
+              Date
+            </Text>
+            <TextInput
+              placeholder="dd/mm/yy"
+              placeholderTextColor={"#9B9B9B"}
+              style={styles.textInput}
+              editable={false}
+              value={selectedDate}
+            />
+          </View>
+        </Pressable>
+        <Pressable onPress={openCategoryModal}>
+          <View style={styles.inputBox}>
+            <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
+              Category
+            </Text>
+            <TextInput
+              placeholder="Choose Category"
+              placeholderTextColor={"#9B9B9B"}
+              style={styles.textInput}
+              editable={false}
+              value={pickedCategories?.name}
+            />
+          </View>
+        </Pressable>
+        <Pressable onPress={openAccountModal}>
+          <View style={styles.inputBox}>
+            <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
+              Account
+            </Text>
+            <TextInput
+              placeholder="Choose Account"
+              placeholderTextColor={"#9B9B9B"}
+              style={styles.textInput}
+              editable={false}
+              value={pickedAccount?.name}
+            />
+          </View>
+        </Pressable>
         <View style={styles.inputBox}>
           <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
             Amount
@@ -43,7 +104,8 @@ export default function AddExpense() {
             placeholder="Insert Amount"
             placeholderTextColor={"#9B9B9B"}
             style={styles.textInput}
-          ></TextInput>
+            keyboardType="numeric"
+          />
         </View>
         <View style={styles.inputBox}>
           <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
@@ -53,9 +115,44 @@ export default function AddExpense() {
             placeholder="Add Note"
             placeholderTextColor={"#9B9B9B"}
             style={styles.textInput}
-          ></TextInput>
+            maxLength={30}
+          />
         </View>
       </View>
+      <TouchableOpacity
+        style={styles.saveButton}
+        onPress={() => nav.navigate("Default")}
+        activeOpacity={0.5}
+      >
+        <Text style={{ color: "#FFFFFF", fontSize: 14, fontWeight: 700 }}>
+          Save
+        </Text>
+      </TouchableOpacity>
+
+      <DateTimePickerModal
+        date={date}
+        isVisible={isPickerVisible}
+        mode="date"
+        onConfirm={handleDateConfirm}
+        onCancel={hideDatePicker}
+      />
+
+      <ContentPicker
+        isvisible={isCategoryModalVisible}
+        onClose={closeCategoryModal}
+      >
+        <Category
+          onSelect={handleCategorySelect}
+          onClose={closeCategoryModal}
+        />
+      </ContentPicker>
+
+      <ContentPicker
+        isvisible={isAccountModalVisible}
+        onClose={closeAccountModal}
+      >
+        <Account onSelect={handleAccountSelect} onClose={closeAccountModal} />
+      </ContentPicker>
     </View>
   );
 }
@@ -83,5 +180,16 @@ const styles = StyleSheet.create({
     marginRight: 25,
     width: "40%",
     textAlign: "right",
+    color: "#000000",
+  },
+  saveButton: {
+    width: "86.4%",
+    height: 36,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    backgroundColor: "#2340DC",
+    marginTop: 38,
   },
 });
--- final-project/app/screens/AddIncome.js
+++ final-project/app/screens/AddIncome.js
@@ -1,31 +1,80 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { TextInput } from "react-native-gesture-handler";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  Keyboard,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+import ContentPicker from "../components/ContentPicker";
+import Account from "../components/Account";
 
 export default function AddIncome() {
+  const [isPickerVisible, setPickerVisible] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState("");
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [pickedCategories, setPickedCategories] = useState(null);
+
+  const openModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
+
+  const showDatePicker = () => {
+    setPickerVisible(true);
+    Keyboard.dismiss();
+  };
+
+  const hideDatePicker = () => setPickerVisible(false);
+
+  const handleCategorySelect = (category) => setPickedCategories(category);
+  const handleDateConfirm = (selectedDate) => {
+    hideDatePicker();
+    setDate(selectedDate);
+    setSelectedDate(
+      `${String(selectedDate.getDate()).padStart(2, "0")}/${String(
+        selectedDate.getMonth() + 1
+      ).padStart(2, "0")}/${String(selectedDate.getFullYear()).slice(2)}`
+    );
+  };
+
+  const nav = useNavigation();
+
   return (
     <View style={styles.base}>
       <View style={{ marginTop: 38 }}>
-        <View style={styles.inputBox}>
-          <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
-            Date
-          </Text>
-          <TextInput
-            placeholder="dd/mm/yy"
-            placeholderTextColor={"#9B9B9B"}
-            style={styles.textInput}
-          ></TextInput>
-        </View>
-        <View style={styles.inputBox}>
-          <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
-            Account
-          </Text>
-          <TextInput
-            placeholder="Choose Account"
-            placeholderTextColor={"#9B9B9B"}
-            style={styles.textInput}
-          ></TextInput>
-        </View>
+        <Pressable onPress={showDatePicker}>
+          <View style={styles.inputBox}>
+            <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
+              Date
+            </Text>
+            <TextInput
+              placeholder="dd/mm/yy"
+              placeholderTextColor={"#9B9B9B"}
+              style={styles.textInput}
+              editable={false}
+              value={selectedDate}
+            />
+          </View>
+        </Pressable>
+        <Pressable onPress={openModal}>
+          <View style={styles.inputBox}>
+            <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
+              Account
+            </Text>
+            <TextInput
+              placeholder="Choose Account"
+              placeholderTextColor={"#9B9B9B"}
+              style={styles.textInput}
+              value={pickedCategories?.name}
+              editable={false}
+            />
+          </View>
+        </Pressable>
         <View style={styles.inputBox}>
           <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
             Amount
@@ -34,7 +83,8 @@ export default function AddIncome() {
             placeholder="Insert Amount"
             placeholderTextColor={"#9B9B9B"}
             style={styles.textInput}
-          ></TextInput>
+            keyboardType="numeric"
+          />
         </View>
         <View style={styles.inputBox}>
           <Text style={{ fontSize: 15, fontWeight: 400, marginLeft: 25 }}>
@@ -44,9 +94,31 @@ export default function AddIncome() {
             placeholder="Add Note"
             placeholderTextColor={"#9B9B9B"}
             style={styles.textInput}
-          ></TextInput>
+            maxLength={30}
+          />
         </View>
+        <TouchableOpacity
+          style={styles.saveButton}
+          onPress={() => nav.navigate("Default")}
+          activeOpacity={0.5}
+        >
+          <Text style={{ color: "#FFFFFF", fontSize: 14, fontWeight: 700 }}>
+            Save
+          </Text>
+        </TouchableOpacity>
       </View>
+
+      <DateTimePickerModal
+        date={date}
+        isVisible={isPickerVisible}
+        mode="date"
+        onConfirm={handleDateConfirm}
+        onCancel={hideDatePicker}
+      />
+
+      <ContentPicker isvisible={isModalVisible} onClose={closeModal}>
+        <Account onSelect={handleCategorySelect} onClose={closeModal} />
+      </ContentPicker>
     </View>
   );
 }
@@ -74,5 +146,16 @@ const styles = StyleSheet.create({
     marginRight: 25,
     width: "40%",
     textAlign: "right",
+    color: "#000000",
+  },
+  saveButton: {
+    width: "86.4%",
+    height: 36,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    backgroundColor: "#2340DC",
+    marginTop: 99,
   },
 });
