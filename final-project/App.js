5dd40b86cde5584fca6e061c58b3af213cdd8c26
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-26 00:18:23 +0700

    linked store

--- final-project/App.js
+++ final-project/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { Provider } from "react-redux";
 
+import store from "./app/redux/store";
 import Navbar from "./app/components/Navbar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Navbar />
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Navbar />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
