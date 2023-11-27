479b08ba9a66f2dff493cf9696199f23260b0d37
davinjason <febrianoahdannadif@mail.ugm.ac.id>
2023-11-27 15:02:59 +0700

    added username initializer

--- final-project/app/redux/actions.js
+++ final-project/app/redux/actions.js
@@ -2,3 +2,8 @@ export const addTransaction = (transaction) => ({
   type: "ADD_TRANSACTION",
   payload: transaction,
 });
+
+export const initUsername = (username) => ({
+  type: "INIT_USERNAME",
+  payload: username,
+});
