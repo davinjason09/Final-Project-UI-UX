51d0bbd5b67536d4869918ae6a84ffeb9c6504fa
davinjason <febrianoahdannadif@mail.ugm.ac.id>
2023-11-30 05:04:57 +0700

    added scroll reset

--- final-project/app/screens/HomePage.js
+++ final-project/app/screens/HomePage.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
 import QuickAccess from "../components/QuickAccess";
 import RecentTransaction from "../components/RecentTransaction";
 
 export default function HomePage() {
+  const scrollViewRef = useRef();
+
+  useFocusEffect(() => {
+    scrollViewRef.current?.scrollTo({ y: 0, animated: true });
+  });
+
   return (
     <View style={styles.base}>
       <ScrollView
+        ref={scrollViewRef}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ flexGrow: 1 }}
       >
