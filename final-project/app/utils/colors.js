10e7d52712036dff9196e03b40efb971c815bbcc
davinjason <ririthcoshyy@gmail.com>
2023-11-29 13:26:44 +0700

    new colors file

---
+++ final-project/app/utils/colors.js
@@ -0,0 +1,19 @@
+let colors = {};
+
+colors.black = "#1D232E";
+colors.blue = "#2340DC";
+colors.blue2 = "#D6DCF6";
+colors.green = "#7BBB71";
+colors.green2 = "#EAFFE7";
+colors.red = "#E73E72";
+colors.red2 = "#FDDCDC";
+colors.red3 = "#EB2020";
+colors.yellow = "#D6DA23";
+colors.yellow2 = "#F5F6BC";
+colors.orange = "#FFA91A";
+colors.orange2 = "#FFF4E0";
+colors.white = "#FFFFFF";
+colors.grey = "#D9D9D9";
+colors.grey2 = "#9B9B9B";
+
+export default colors;
