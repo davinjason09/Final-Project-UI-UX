cca3166e43e917d0b0006cbfe89c02a0a5d47f47
davinjason <aisyahputrikhurinin@mail.ugm.ac.id>
2023-11-29 09:09:19 +0700

    added resetDate and changeDate

--- final-project/app/redux/actions.js
+++ final-project/app/redux/actions.js
@@ -7,3 +7,13 @@ export const initUsername = (username) => ({
   type: "INIT_USERNAME",
   payload: username,
 });
+
+export const resetDate = (month, year) => ({
+  type: "RESET_DATE",
+  payload: { month, year },
+});
+
+export const changeDate = (month, year) => ({
+  type: "CHANGE_DATE",
+  payload: { month, year },
+});
