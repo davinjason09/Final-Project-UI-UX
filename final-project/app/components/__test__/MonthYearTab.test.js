73a681406f779e496dae8b1ae46dd0ac4a305b17
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-30 20:09:51 +0700

    unit test

---
+++ final-project/app/components/__test__/MonthYearTab.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import MonthYearTab from "../MonthYearTab";
+import { changeDate } from "../../redux/actions";
+
+const mockStore = configureStore([]);
+
+describe("MonthYearTab", () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      initialMonth: 11,
+      initialYear: 2023,
+    });
+  });
+
+  it("should handle previous month correctly", () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <MonthYearTab />
+      </Provider>
+    );
+
+    fireEvent.press(getByTestId("prevButton"));
+
+    const actions = store.getActions();
+    expect(actions).toEqual([changeDate(10, 2023)]);
+  });
+
+  it("should handle next month correctly", () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <MonthYearTab />
+      </Provider>
+    );
+
+    fireEvent.press(getByTestId("nextButton"));
+
+    const actions = store.getActions();
+    expect(actions).toEqual([changeDate(12, 2023)]);
+  });
+});
